a1ed0a9f7c96c33d6458cd3b7fff9ef1a71e2c81
yudai1202 <u.yudai1202@gmail.com>
2021-01-28 20:00:14 +0900

    feat: create Template

--- src/components/Templates/Template.tsx
+++ src/components/Templates/Template.tsx
@@ -1,7 +1,20 @@
 import * as React from "react";
+import Header from "@/components/Organisms/Header";
+import Main from "@/components/Organisms/Main";
+import Footer from "@/components/Organisms/Footer";
 
-const Template = (): JSX.Element => {
-  return <h1>Hello Template</h1>;
+type IProps = {
+  children?: React.ReactNode;
+} & React.ComponentProps<typeof Header>;
+
+const Template = ({ children, title }: IProps): JSX.Element => {
+  return (
+    <React.Fragment>
+      <Header title={title} />
+      <Main>{children}</Main>
+      <Footer />
+    </React.Fragment>
+  );
 };
 
 export default Template;
