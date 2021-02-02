1e30886d65e2f8fe15823c7b6ebb94b9db5a501d
yudai1202 <u.yudai1202@gmail.com>
2021-02-02 17:09:14 +0900

    feat: header tag type

--- src/components/Organisms/Header.tsx
+++ src/components/Organisms/Header.tsx
@@ -3,17 +3,25 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import { Typography } from "@material-ui/core";
 import Link from "next/link";
+import { useRouter } from "next/router";
+
 type IProps = {
   title: string;
 };
 
 const Header = ({ title }: IProps): JSX.Element => {
+  const { pathname } = useRouter();
+
   return (
     <React.Fragment>
       <AppBar position={"static"}>
         <Toolbar>
           <Link href="/">
-            <Typography component="h1" variant="h5" css={{ cursor: "pointer" }}>
+            <Typography
+              component={pathname === "/" ? "h1" : "div"}
+              variant="h5"
+              css={{ cursor: "pointer" }}
+            >
               {title}
             </Typography>
           </Link>
