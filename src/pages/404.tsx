da81be4134bbb1c37149bbc6e85135d016761d3e
yudai1202 <u.yudai1202@gmail.com>
2021-02-04 17:13:14 +0900

    style: centering message

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,4 +1,6 @@
-import { Box, Center, Flex, Heading } from "@chakra-ui/react";
+import * as React from "react";
+import Head from "next/head";
+import { Box, Flex, Heading } from "@chakra-ui/react";
 import Header from "@/components/Organisms/Header";
 import Footer from "@/components/Organisms/Footer";
 
@@ -6,24 +8,27 @@ const NotFound = (): JSX.Element => {
   const blogTitle = "UDlog";
 
   return (
-    <Flex direction="column" minHeight="100vh">
-      <Header title={blogTitle} />
-      <Box position="relative" flex={1}>
-        <Flex
-          justifyContent="center"
-          alignItems="center"
-          position="absolute"
-          top="50%"
-          left="50%"
-          transform="translateY(-50%) translateX(-50%)"
-        >
-          <Center>
-            <Heading as="h1">404 | NotFound</Heading>
-          </Center>
-        </Flex>
-      </Box>
-      <Footer title={blogTitle} />
-    </Flex>
+    <React.Fragment>
+      <Head>
+        <title>Page Not Found - {blogTitle}</title>
+      </Head>
+      <Flex direction="column" minHeight="100vh">
+        <Header title={blogTitle} />
+        <Box position="relative" flexGrow={1}>
+          <Flex
+            justifyContent="center"
+            alignItems="center"
+            position="absolute"
+            top="50%"
+            left="50%"
+            transform="translateY(-50%) translateX(-50%)"
+          >
+            <Heading as="h1">404 | Page Not Found</Heading>
+          </Flex>
+        </Box>
+        <Footer title={blogTitle} />
+      </Flex>
+    </React.Fragment>
   );
 };
 
