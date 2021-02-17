3f653c2318e7ae9ad02a6d33f7790e7f05e4e5b9
yudai1202 <u.yudai1202@gmail.com>
2021-02-17 09:29:43 +0900

    feat: design 404 page by tailwind

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import Head from "next/head";
-import { Box, Flex, Heading } from "@chakra-ui/react";
 import Header from "@/components/Organisms/Header";
 import Footer from "@/components/Organisms/Footer";
 
@@ -12,22 +11,31 @@ const NotFound = (): JSX.Element => {
       <Head>
         <title>Page Not Found - {blogTitle}</title>
       </Head>
-      <Flex direction="column" minHeight="100vh">
+      <div className="flex flex-col min-h-screen">
         <Header title={blogTitle} />
-        <Box position="relative" flexGrow={1}>
-          <Flex
-            justifyContent="center"
-            alignItems="center"
-            position="absolute"
-            top="50%"
-            left="50%"
-            transform="translateY(-50%) translateX(-50%)"
-          >
-            <Heading as="h1">404 | Page Not Found</Heading>
-          </Flex>
-        </Box>
+        <div className="h-screen w-screen bg-gray-100 flex items-center">
+          <div className="container flex flex-col md:flex-row items-center justify-center px-5 text-gray-700">
+            <div className="max-w-md">
+              <div className="text-5xl font-dark font-bold">404</div>
+              <p className="text-2xl md:text-3xl font-light leading-normal">
+                Sorry we couldn&apos;t find this page.
+              </p>
+              <p className="mb-8">
+                But dont worry, you can find plenty of other things on our
+                homepage.
+              </p>
+
+              <a
+                href="/"
+                className="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-blue-600 active:bg-blue-600 hover:bg-blue-700"
+              >
+                back to homepage
+              </a>
+            </div>
+          </div>
+        </div>
         <Footer title={blogTitle} />
-      </Flex>
+      </div>
     </React.Fragment>
   );
 };
