c8ac9e30fb4d003d0d050db9f1759e5908ef45ce
yudai1202 <u.yudai1202@gmail.com>
2021-02-17 09:36:52 +0900

    feat: japanize page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -15,22 +15,22 @@ const NotFound = (): JSX.Element => {
         <Header title={blogTitle} />
         <div className="h-screen w-screen bg-gray-100 flex items-center">
           <div className="container flex flex-col md:flex-row items-center justify-center px-5 text-gray-700">
-            <div className="max-w-md">
+            <div className="max-w-lg">
               <div className="text-5xl font-dark font-bold">404</div>
               <p className="text-2xl md:text-3xl font-light leading-normal">
-                Sorry we couldn&apos;t find this page.
+                ページが見つかりません。
               </p>
-              <p className="mb-8">
-                But dont worry, you can find plenty of other things on our
-                homepage.
+              <p className="mt-2">
+                下のボタンをクリックして、トップページに戻ることができます。
               </p>
-
-              <a
-                href="/"
-                className="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-blue-600 active:bg-blue-600 hover:bg-blue-700"
-              >
-                back to homepage
-              </a>
+              <div className="mt-8">
+                <a
+                  href="/"
+                  className="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-blue-600 active:bg-blue-600 hover:bg-blue-700"
+                >
+                  トップへ戻る
+                </a>
+              </div>
             </div>
           </div>
         </div>
