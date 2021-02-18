0f946fd4c8bdb8151e7efcdb749a93b7730ee425
yudai1202 <u.yudai1202@gmail.com>
2021-02-18 13:24:04 +0900

    fix: 404 page layout

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -13,7 +13,7 @@ const NotFound = (): JSX.Element => {
       </Head>
       <div className="flex flex-col min-h-screen">
         <Header title={blogTitle} />
-        <div className="h-screen w-screen bg-gray-100 flex items-center">
+        <div className="flex-grow w-screen bg-gray-100 flex items-center">
           <div className="container flex flex-col md:flex-row items-center justify-center px-5 text-gray-700">
             <div className="max-w-lg">
               <div className="text-5xl font-dark font-bold">404</div>
